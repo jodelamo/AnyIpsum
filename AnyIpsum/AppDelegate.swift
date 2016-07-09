4fe747a49e47955a1c77c39320aa438bcf6ac82b
Joacim Löwgren <joacim.lowgren@gmail.com>
2016-07-09 07:22:57 +0200

    Initial Commit

---
+++ AnyIpsum/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AnyIpsum
+//
+//  Created by Joacim Löwgren on 09/07/16.
+//  Copyright © 2016 Joacim Löwgren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
