526008982680d3fce81ff060e3e5597fbf5c270c
Joacim de la Motte <joacim.delamotte@gmail.com>
2026-09-19 20:29:01 +0200

    ci: use Xcode 27 runners

--- AnyIpsum/ShortcutManager.swift
+++ AnyIpsum/ShortcutManager.swift
@@ -4,7 +4,7 @@ import KeyboardShortcuts
 extension KeyboardShortcuts.Name {
     static let openMenu = Self(
         "openMenu",
-        default: .init(.a, modifiers: [.control, .command])
+        initial: .init(.a, modifiers: [.control, .command])
     )
 }
 
